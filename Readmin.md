57fd7aa5efb8ac21c01e9ad1087daeea99eeaaab
juan <juan.urgiles@digytronic.com>
2019-07-19 17:32:09 -0500

    Cambio adicionando el envio de correos con gmail

---
+++ Readmin.md
@@ -0,0 +1,8 @@
+Paso 1:
+Activar aplicaciones no seguras en correo de gmail:
+- Iniciar la cuenta de gmail
+- Ingresar en https://myaccount.google.com/lesssecureapps para activar la cuenta.
+
+Paso2: Cambiar en application.properties por
+spring.mail.username=test@gmail.com
+spring.mail.password=Correoprueba
---
+++ src/main/java/ec/edu/tecazuay/microservicio1/servicio/CorreoService.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.tecazuay.microservicio1.servicio;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author casa
+ */
+@Service
+public class CorreoService {
+    @Autowired
+    private JavaMailSender javaMailSender;
+	
+	public void sendEmail() {
+
+            SimpleMailMessage msg = new SimpleMailMessage();
+        msg.setTo("juan.urgilesc@gmail.com");
+
+        msg.setSubject("Testing from Spring Boot");
+        msg.setText("Hello World \n Spring Boot Email");
+
+        javaMailSender.send(msg);
+
+    }
+}
